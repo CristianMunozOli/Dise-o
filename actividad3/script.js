24c644620b4cebe4367a6ed1881916ab2c604fec
cmo_d <230183@fppiramide.com>
2024-01-05 17:53:17 +0100

    Seccion Rap terminada

--- actividad3/script.js
+++ actividad3/script.js
@@ -1,20 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var animatedText1 = document.getElementById('animatedText1');
-    var animatedText2 = document.getElementById('animatedText2');
 
-    // Al cargar la página, inicia la animación del primer div
-    setTimeout(function () {
-      animatedText1.style.opacity = '0';
-      animatedText2.style.transform = 'translateY(0)';
-      animatedText1.style.transform = 'scale(2)';
+  function detenerOtrosAudios(idAudio) {
+    // Obtener todos los elementos de audio en la página
+    let todosLosAudios = document.getElementsByTagName('audio');
 
-    }, 10);
-
-    // Después de que termine la animación del primer div, inicia la animación del segundo div
-    setTimeout(function () {
-      animatedText2.style.opacity = '1';
-      animatedText2.style.transform = 'translateY(0)';
-      animatedText2.style.transform = 'scale(1)';
-      animatedText1.style.display = 'none';
-    }, 5500); // El tiempo total de espera es 500ms (del primer div) + 5900ms (duración total de la animación del primer div)
-  });
+    // Pausar todos los demás audios excepto el especificado
+    for (let i = 0; i < todosLosAudios.length; i++) {
+        let audio = todosLosAudios[i];
+        if (audio.id !== idAudio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    }
+}
